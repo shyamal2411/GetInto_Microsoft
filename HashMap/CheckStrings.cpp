0474fe26300e700118bf9ea0edf17c46d4a41688
Shyamal <sgprajapati24@gmail.com>
2022-04-04 21:13:01 +0530

    check strings

---
+++ HashMap/CheckStrings.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+class Solution{
+    public:
+    bool  check(vector<string> s,vector<string> t){
+        unordered_map<string, int> mp;
+        int n = s.size(), m = t.size();
+        
+    }
+};
+
+int main(){
+    Solution ss;
+    vector<string> s = {"abc", "d"};
+    vector<string> t = {"ab", "cd"};
+    cout<<ss.check(s, t);
+    return 0;
+}
