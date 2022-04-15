64efb4499e039c73d05b075fa04f5adb2658f1ff
Shyamal <sgprajapati24@gmail.com>
2022-04-15 22:54:36 +0530

    set zero matrix - better approaches

--- Striver/Day_1/1_SetZeroMatrix.cpp
+++ Striver/Day_1/1_SetZeroMatrix.cpp
@@ -57,6 +57,58 @@ class Solution{
 };
 
 
+class Solution2 {
+public:
+    void setZeroes(vector<vector<int>>& mat) {
+        int row = mat.size(), col = mat[0].size();
+        vector<int> dum1(row, -1), dum2(col, -1);
+        
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(mat[i][j] == 0){
+                 dum1[i] = 0;
+                 dum2[j] = 0;
+                }                    
+            }
+        }
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(dum1[i] == 0 || dum2[j] == 0){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+class Solution3{
+    public:
+    void setZeroes(vector<vector<int>> &mat){
+        int col0 = 1, row = mat.size(), col = mat[0].size();
+        for(int i = 0; i < row; i++){
+            if(mat[i][0] == 0) col0 = 0;
+
+            for(int j = 1; j < col; j++){
+                if(mat[i][0] == 0){
+                    mat[i][0] = 0;
+                    mat[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = row - 1; i >= 0; i--){
+            for(int j = col - 1; j >= 1; j--){
+                if(mat[i][0] == 0 || mat[0][j] == 0){
+                    mat[i][j] = 0;
+                }
+            }
+            if(col0 == 0) mat[i][0] = 0;
+        }
+    }
+};
+
+
 int main(){
     Solution ss;
     vector<vector<int>> vec = {{1,2,3}, {1, 0, 1}, {1, 2, 3}};
